ea0e4eaab6fda04ceed25134e0ea9202ab590ebf
rawanntarek <rawaaan245@gmail.com>
2023-12-30 09:02:17 +0200

    .

--- OrdersAndNotificationsManager/src/main/java/com/example/OrdersAndNotificationsManager/Notifications/NotificationService.java
+++ OrdersAndNotificationsManager/src/main/java/com/example/OrdersAndNotificationsManager/Notifications/NotificationService.java
@@ -1,9 +1,15 @@
 package com.example.OrdersAndNotificationsManager.Notifications;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NotificationService implements NotificationSubject {
+
+    List<NotificationObserver> observers=new ArrayList<>();
+    String mainState;
     @Override
     public void attach(NotificationObserver observer) {
-
+        
     }
 
     @Override
