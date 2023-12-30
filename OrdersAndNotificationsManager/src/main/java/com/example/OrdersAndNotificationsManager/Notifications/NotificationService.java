eeecc76511dc7d5c96c59101302a141e013a3424
rawanntarek <rawaaan245@gmail.com>
2023-12-30 08:23:36 +0200

    .

---
+++ OrdersAndNotificationsManager/src/main/java/com/example/OrdersAndNotificationsManager/Notifications/NotificationService.java
@@ -0,0 +1,18 @@
+package com.example.OrdersAndNotificationsManager.Notifications;
+
+public class NotificationService implements NotificationSubject {
+    @Override
+    public void attach(NotificationObserver observer) {
+
+    }
+
+    @Override
+    public void detach(NotificationObserver observer) {
+
+    }
+
+    @Override
+    public void notifyObservers(String notification) {
+
+    }
+}
